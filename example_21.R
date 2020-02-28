50d567f4db96d7555d56d9829b4073dcdf8300ac
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 13:24:45 +0100

    EOD

--- example_21.R
+++ example_21.R
@@ -32,9 +32,15 @@ testthat::expect_error(seed_to_sequence_length(322))
 for (rng_seed in seq(314, 321)) {
   folder_name <- file.path(paste0("example_", example_no, "_", rng_seed))
 
+  ##############################################################################
+  # Create phylogeny
+  ##############################################################################
   set.seed(rng_seed)
   phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
 
+  ##############################################################################
+  # Setup pirouette
+  ##############################################################################
   pir_params <- create_std_pir_params(
     folder_name = folder_name,
     sequence_length = seed_to_sequence_length(rng_seed)
