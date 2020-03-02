af535d88eab60b9b9bf57e3e80b3cf2eb0e5374e
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 14:15:54 +0100

    Nice code

--- example_21.R
+++ example_21.R
@@ -2,52 +2,65 @@
 #
 # Write script that shows the true and twin error for hundreds of trees with
 # 10, 1k, 2k, ..., 10k nucleotides
-suppressMessages(library(pirouette))
+library(pirouette)
 
 # Constants
-is_testing <- is_on_travis()
-root_folder <- getwd()
 example_no <- 21
-seed_to_sequence_length <- function(rng_seed) {
-  sequence_lengths <- c(100, 248, 500, 1000, 2000, 4000, 8000, 16000)
-  sequence_length <- sequence_lengths[rng_seed + 1 - 314]
-  if (is.na(sequence_length)) stop("Invalid seed")
-  sequence_length
+rng_seed <- 314
+crown_age <- 10
+sequence_lengths <- c(100, 248, 500, 1000, 2000, 4000, 8000, 16000)
+n_phylogenies_per_sequence_length <- 5
+is_testing <- is_on_travis()
+if (is_testing) {
+  sequence_lengths <- c(100, 248)
+  n_phylogenies_per_sequence_length <- 2
 }
-testthat::expect_equal(seed_to_sequence_length(314), 100)
-testthat::expect_equal(seed_to_sequence_length(315), 248)
-testthat::expect_equal(seed_to_sequence_length(316), 500)
-testthat::expect_equal(seed_to_sequence_length(317), 1000)
-testthat::expect_equal(seed_to_sequence_length(318), 2000)
-testthat::expect_equal(seed_to_sequence_length(319), 4000)
-testthat::expect_equal(seed_to_sequence_length(320), 8000)
-testthat::expect_equal(seed_to_sequence_length(321), 16000)
-testthat::expect_error(seed_to_sequence_length(322))
-for (rng_seed in seq(314, 321)) {
-  folder_name <- file.path(paste0("example_", example_no, "_", rng_seed))
-  ##############################################################################
-  # Create phylogeny
-  ##############################################################################
+n_sequence_lengths <- length(sequence_lengths)
+n_pir_params <- n_sequence_lengths * n_phylogenies_per_sequence_length
+
+# Create phylogenies
+phylogenies <- list()
+for (i in seq_len(n_phylogenies_per_sequence_length)) {
   set.seed(rng_seed)
   phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
-  ##############################################################################
-  # Setup pirouette
-  ##############################################################################
-  pir_params <- create_std_pir_params(
-    folder_name = folder_name,
-    sequence_length = seed_to_sequence_length(rng_seed)
+  phylogenies[[i]] <- phylogeny
+}
+# 1 2 3 1 2 3
+phylogenies <- rep(phylogenies, n_phylogenies_per_sequence_length)
+
+# Create pirouette parameter sets
+pir_paramses <- create_std_pir_paramses(n = n_pir_params)
+expect_equal(length(pir_paramses), length(phylogenies))
+if (is_testing) {
+  pir_paramses <- shorten_pir_params(pir_paramses)
+}
+
+# Set the alignment lengths
+# 1 1 1 2 2 2
+sequence_lengthses <- rep(
+  sequence_lengths, each = n_phylogenies_per_sequence_length
+)
+expect_equal(length(sequence_lengthses), length(pir_paramses))
+for (i in seq_along(sequence_lengthses)) {
+  pir_paramses[[i]]$alignment_params$root_sequence <- create_blocked_dna(
+    length =  sequence_lengthses[[i]]
   )
-  if (is_testing) {
-    pir_params <- shorten_pir_params(pir_params)
-  }
-  pir_out <- pir_run(
-    phylogeny = phylogeny,
-    pir_params = pir_params
-  )
+}
+
+# Do the runs
+pir_outs <- pir_runs(
+  phylogenies = phylogenies,
+  pir_paramses = pir_paramses
+)
+
+# Save
+expect_equal(length(pir_paramses), length(pir_outs))
+expect_equal(length(pir_paramses), length(phylogenies))
+for (i in seq_along(pir_outs)) {
   pir_save(
-    phylogeny = phylogeny,
-    pir_params = pir_params,
-    pir_out = pir_out,
-    folder_name = folder_name
+    phylogeny = phylogenies[[i]],
+    pir_params = pir_paramses[[i]],
+    pir_out = pir_outs[[i]],
+    folder_name = dirname(pir_paramses[[i]]$alignment_params$fasta_filename)
   )
 }
